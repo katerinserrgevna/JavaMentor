80bf9b31ec181c8ccfd57e96a2a35585ac5a4e35
katerinserrgevna <64695266+katerinserrgevna@users.noreply.github.com>
2021-03-15 01:29:30 +0300

    Delete

--- calc/calc/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String argv[]){
-        Scanner in = new Scanner(System.in);
-        Parser pars = new Parser(in.nextLine());
-        pars.pars();
-    }
-}
